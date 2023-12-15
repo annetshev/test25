a804df1ae9b4eb839a4100b2f1064bb7a90db680
AnnetShev <shevanna03@gmail.com>
2023-12-15 11:09:37 +0200

    pz 2_11

--- script.js
+++ script.js
@@ -2,28 +2,52 @@ document.addEventListener('DOMContentLoaded', function () {
     const characterListContainer = document.getElementById('character-list');
     const characterInfoContainer = document.getElementById('character-info');
     const apiUrl = 'https://anapioficeandfire.com/api/characters';
+    let currentPage = 1;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => displayCharacterList(data))
-        .catch(error => console.error('Помилка отримання даних:', error));
+    function httpRequest(type, url, options) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open(type, url);
+            xhr.onload = function () {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject(new Error(`Помилка отримання даних: ${xhr.statusText}`));
+                }
+            };
+            xhr.onerror = function () {
+                reject(new Error('Помилка отримання даних'));
+            };
+            xhr.send(options);
+        });
+    }
+
+    function loadMoreCharacters() {
+        currentPage++;
+        const url = `${apiUrl}?page=${currentPage}`;
+        httpRequest('GET', url)
+            .then(data => displayCharacterList(data))
+            .catch(error => console.error('Помилка отримання даних:', error));
+    }
 
     function displayCharacterList(characters) {
+        characterListContainer.innerHTML = '';
+    
         characters.forEach((character, index) => {
             const characterElement = document.createElement('div');
             characterElement.classList.add('character');
             characterElement.textContent = `character ${index + 1}`;
-
+    
             characterElement.addEventListener('click', () => {
                 displayCharacterInfo(character);
                 setActiveCharacter(characterElement);
             });
-
+    
             characterListContainer.appendChild(characterElement);
         });
     }
-
     function displayCharacterInfo(character) {
+
         const markup = `
         <h1>${character.name}</h1>
         <h2>gender: ${character.gender} culture: ${character.culture}</h2>
@@ -42,12 +66,21 @@ document.addEventListener('DOMContentLoaded', function () {
         characterInfoContainer.innerHTML = markup;
     }
 
+
     function setActiveCharacter(selectedCharacterElement) {
         const characterElements = document.querySelectorAll('.character');
         characterElements.forEach(element => {
-            element.style.backgroundColor = '#808080'; // Сірий фон для всіх персонажів
+            element.style.backgroundColor = '#808080';
         });
 
-        selectedCharacterElement.style.backgroundColor = '#fff'; // Білий фон для вибраного персонажа
+        selectedCharacterElement.style.backgroundColor = '#fff';
     }
+
+    window.addEventListener('scroll', function () {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            loadMoreCharacters();
+        }
+    });
+
+    loadMoreCharacters();
 });
